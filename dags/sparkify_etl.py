a6540a8d9235835dbaa787ed1baf87f1b12016f5
lastnode <lastnode@users.noreply.github.com>
2021-02-20 07:40:05 +0530

    Made DAG hourly

--- dags/sparkify_etl.py
+++ dags/sparkify_etl.py
@@ -23,7 +23,7 @@ default_args = {
 dag = DAG('sparkify_etl',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 0 * * *',
+          schedule_interval='0 * * * *',
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
